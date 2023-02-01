c705ee5313848f68c99d6ca8e9bf0f1780240d91
krishanmohan1002 <krishanmohan1002@gmail.com>
2023-02-01 17:56:32 +0530

    first commit

--- src/main/java/com/app/rahul/RahulBorabandaRestApiCrudExOld1Application.java
+++ src/main/java/com/app/rahul/RahulBorabandaRestApiCrudExOld1Application.java
@@ -9,10 +9,4 @@ public class RahulBorabandaRestApiCrudExOld1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(RahulBorabandaRestApiCrudExOld1Application.class, args);
 	}
-
-	@Configuration
-	public ModelMapper(){
-	  return new ModelMapper();
-	}
-
 }
